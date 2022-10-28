a72383f3f0d737f08d8ae1f5f94fb778f37737dd
jrrc68 <116301769+jrrc68@users.noreply.github.com>
2022-10-28 20:29:35 +0200

    Primeras pruebas del módulo DFPlayers

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+dfplayermini.connect(SerialPin.P13, SerialPin.P14)
+dfplayermini.playFileInFolder(1, 1, dfplayermini.isRepeat.No)
 basic.forever(function () {
-	
+    basic.showNumber(pins.analogReadPin(AnalogPin.P0))
 })
