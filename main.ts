1e8d9c275b0e7380bbc93488efb5af52bf23e41f
jrrc68 <116301769+jrrc68@users.noreply.github.com>
2022-10-30 21:59:42 +0100

    Teclado analógico y  detección de secuencia

--- main.ts
+++ main.ts
@@ -1,5 +1,50 @@
+function SECUENCIA (letra: string, carpeta: number, archivo: number, key: number) {
+    if (analogic_key == key) {
+        secuencia = "" + secuencia + letra
+        dfplayermini.playFileInFolder(carpeta, archivo, dfplayermini.isRepeat.No)
+        basic.showNumber(analogic_key)
+    }
+}
+let analogic_key = 0
+let secuencia = ""
+secuencia = ""
 dfplayermini.connect(SerialPin.P13, SerialPin.P14)
-dfplayermini.playFileInFolder(1, 1, dfplayermini.isRepeat.No)
+dfplayermini.press(dfplayermini.playType.Stop)
 basic.forever(function () {
-    basic.showNumber(pins.analogReadPin(AnalogPin.P0))
+    analogic_key = Math.trunc(pins.map(
+    pins.analogReadPin(AnalogPin.P0),
+    0,
+    1023,
+    0,
+    7
+    ))
+    SECUENCIA("a", 1, 1, 1)
+    SECUENCIA("b", 1, 3, 2)
+    SECUENCIA("c", 1, 5, 3)
+    SECUENCIA("d", 2, 2, 4)
+    SECUENCIA("e", 2, 4, 5)
+    SECUENCIA("f", 2, 4, 6)
+    if (secuencia.length >= 6) {
+        if (secuencia == "abcdef") {
+            basic.showLeds(`
+                # # # # #
+                # . . . #
+                # . . . #
+                # . . . #
+                # # # # #
+                `)
+        } else {
+            basic.showLeds(`
+                # . . . #
+                . # . # .
+                . . # . .
+                . # . # .
+                # . . . #
+                `)
+        }
+        dfplayermini.press(dfplayermini.playType.Stop)
+        basic.pause(1000)
+        secuencia = ""
+        basic.clearScreen()
+    }
 })
